22f3f83f03c011b7f8b3d8f4c0a15ab13cd707d6
dja1716 <hdum@uwaterloo.ca>
2021-11-02 16:07:43 +0900

    Apploading 4

--- App.js
+++ App.js
@@ -3,29 +3,13 @@ import { View, Text, Image } from "react-native";
 import AppLoading from "expo-app-loading";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
-import { Asset } from "expo-asset";
+import { Asset, useAssets } from "expo-asset";
 
 export default function App() {
-  const [ready, setReady] = useState(false);
-  const onFinish = () => setReady(true);
-  const startLoading = async () => {
-    await Font.loadAsync(Ionicons.font);
-    await Asset.loadAsync(require("./dummy.jpeg")); // local filesystem
-    await Image.prefetch(
-      "https://res.cloudinary.com/sitereq-production/image/upload/PostContentImage/565x380/Android%20Studio0716201912541712252019083500"
-    ); // on the app
-    // load everything here;
-    // video, database - preload
-    await new Promise((resolve) => setTimeout(resolve, 10000));
-  };
-  if (!ready) {
-    return (
-      <AppLoading
-        startAsync={startLoading}
-        onFinish={onFinish}
-        onError={console.error}
-      />
-    );
+  const [assets] = useAssets([require("./dummy.jpeg")]);
+  const [loaded] = Font.useFonts(Ionicons.font);
+  if (!assets || !loaded) {
+    return <AppLoading />;
   }
   return <Text>eeeeeeeewe are done loading</Text>;
 }
