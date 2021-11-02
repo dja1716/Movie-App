c5ed5eeba8325fe7e78eb5279ebc17b28f3d58ba
dja1716 <hdum@uwaterloo.ca>
2021-11-02 20:53:19 +0900

    Theme added

--- App.js
+++ App.js
@@ -4,13 +4,11 @@ import AppLoading from "expo-app-loading";
 import * as Font from "expo-font";
 import { Ionicons } from "@expo/vector-icons";
 import { Asset, useAssets } from "expo-asset";
-import {
-  NavigationContainer,
-  DarkTheme,
-  DefaultTheme,
-} from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./navigation/Tabs";
 import Root from "./navigation/Root";
+import { ThemeProvider } from "styled-components/native";
+import { darkTheme, lightTheme } from "./styled";
 
 const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
 
@@ -42,8 +40,10 @@ export default function App() {
     );
   }
   return (
-    <NavigationContainer>
-      <Root />
-    </NavigationContainer>
+    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
+      <NavigationContainer>
+        <Root />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
--- screens/Movies.js
+++ screens/Movies.js
@@ -1,13 +1,27 @@
 import React from "react";
-import { TouchableOpacity, Text } from "react-native";
+import styled from "styled-components/native";
 
 const Movies = ({ navigation: { navigate } }) => (
-  <TouchableOpacity
-    onPress={() => navigate("Stack", { screen: "Three" })}
-    style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
-  >
-    <Text>Movies</Text>
-  </TouchableOpacity>
+  <Btn onPress={() => navigate("Stack", { screen: "Three" })}>
+    <Title>Movies</Title>
+  </Btn>
 );
 
 export default Movies;
+
+const Title = styled.Text`
+  color: ${(props) => props.theme.textColor};
+`;
+
+const Btn = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: ${(props) => props.theme.mainBgColor};
+`;
+
+const Header = styled.View``;
+
+const Column = styled.View``;
+
+const Footer = styled.View``;
