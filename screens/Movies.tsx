4e43c8eb2b5306dbda8cb0ff4b73f66530ded460
dja1716 <hdum@uwaterloo.ca>
2021-11-02 22:20:30 +0900

    movie nowPlaying 1

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,30 +1,42 @@
 import React from "react";
 import styled from "styled-components/native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import Swiper from "react-native-web-swiper";
+import { Dimensions } from "react-native";
 
-const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = ({
-  navigation: { navigate },
-}) => (
-  <Btn onPress={() => navigate("Stack", { screen: "Three" })}>
-    <Title>Movies</Title>
-  </Btn>
-);
+const API_KEY = "356723332b0147990f77bce41e0a96bb";
+
+const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
+  const getNowPlaying = () => {
+    fetch(
+      `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`
+    );
+  };
+  return (
+    <Container>
+      <Swiper
+        loop
+        timeout={3.5}
+        controlsEnabled={false}
+        containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
+      >
+        <View style={{ backgroundColor: "red" }}></View>
+        <View style={{ backgroundColor: "blue" }}></View>
+        <View style={{ backgroundColor: "red" }}></View>
+        <View style={{ backgroundColor: "blue" }}></View>
+      </Swiper>
+    </Container>
+  );
+};
 
 export default Movies;
 
-const Title = styled.Text`
-  color: ${(props) => props.theme.textColor};
-`;
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
-const Btn = styled.TouchableOpacity`
-  flex: 1;
-  justify-content: center;
-  align-items: center;
+const Container = styled.ScrollView`
   background-color: ${(props) => props.theme.mainBgColor};
 `;
 
-const Header = styled.View``;
-
-const Column = styled.View``;
-
-const Footer = styled.View``;
+const View = styled.View`
+  flex: 1;
+`;
