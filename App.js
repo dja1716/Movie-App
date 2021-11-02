e3e1d34d251e4c652b046cb7c872e032d90c6b44
dja1716 <hdum@uwaterloo.ca>
2021-11-02 15:28:26 +0900

    Apploading 2

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import React, { useState } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset } from "expo-asset";
 
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true);
   const startLoading = async () => {
+    await Font.loadAsync(Ionicons.font);
+    await Asset.loadAsync(require("./dummy.jpeg")); // local filesystem
+    await Image.prefetch(
+      "https://res.cloudinary.com/sitereq-production/image/upload/PostContentImage/565x380/Android%20Studio0716201912541712252019083500"
+    ); // on the app
     // load everything here;
     // video, database - preload
     await new Promise((resolve) => setTimeout(resolve, 10000));
@@ -19,5 +27,5 @@ export default function App() {
       />
     );
   }
-  return <Text>we are done loading</Text>;
+  return <Text>eeeeeeeewe are done loading</Text>;
 }
