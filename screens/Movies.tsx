ece00e162d144c193d2e6875cce9fb2d85237249
dja1716 <hdum@uwaterloo.ca>
2021-11-03 12:56:51 +0900

    Movie: nowPlaying

--- screens/Movies.tsx
+++ screens/Movies.tsx
@@ -1,29 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components/native";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import Swiper from "react-native-web-swiper";
-import { Dimensions } from "react-native";
+import Swiper from "react-native-swiper";
+import {
+  ActivityIndicator,
+  Dimensions,
+  StyleSheet,
+  useColorScheme,
+} from "react-native";
+import { makeImgPath } from "../utils";
+import { BlurView } from "expo-blur";
 
 const API_KEY = "356723332b0147990f77bce41e0a96bb";
 
 const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
-  const getNowPlaying = () => {
-    fetch(
-      `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`
-    );
+  const isDark = useColorScheme() === "dark";
+  const [loading, setLoading] = useState(true);
+  const [nowPlaying, setNowPlaying] = useState([]);
+  const getNowPlaying = async () => {
+    const { results } = await (
+      await fetch(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`
+      )
+    ).json();
+    setNowPlaying(results);
+    setLoading(false);
   };
-  return (
+  useEffect(() => {
+    getNowPlaying();
+  }, []);
+  return loading ? (
+    <Loader>
+      <ActivityIndicator size="small" />
+    </Loader>
+  ) : (
     <Container>
       <Swiper
         loop
-        timeout={3.5}
-        controlsEnabled={false}
+        horizontal
+        autoplay
+        autoplayTimeout={3.5}
+        showsButtons={false}
+        showsPagination={false}
         containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4 }}
       >
-        <View style={{ backgroundColor: "red" }}></View>
-        <View style={{ backgroundColor: "blue" }}></View>
-        <View style={{ backgroundColor: "red" }}></View>
-        <View style={{ backgroundColor: "blue" }}></View>
+        {nowPlaying.map((movie) => (
+          <View key={movie.id}>
+            <BgImg
+              style={StyleSheet.absoluteFill}
+              source={{ uri: makeImgPath(movie.backdrop_path) }}
+            />
+            <BlurView
+              tint={isDark ? "dark" : "light"}
+              intensity={80}
+              style={StyleSheet.absoluteFill}
+            >
+              <Wrapper>
+                <Poster source={{ uri: makeImgPath(movie.poster_path) }} />
+                <Column>
+                  <Title>{movie.original_title}</Title>
+                  {movie.vote_average > 0 ? (
+                    <Votes>⭐{movie.vote_average}/10</Votes>
+                  ) : null}
+                  <Overview>{movie.overview.slice(0, 90)}...</Overview>
+                </Column>
+              </Wrapper>
+            </BlurView>
+          </View>
+        ))}
       </Swiper>
     </Container>
   );
@@ -33,10 +77,50 @@ export default Movies;
 
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
-const Container = styled.ScrollView`
-  background-color: ${(props) => props.theme.mainBgColor};
-`;
+const Container = styled.ScrollView``;
 
 const View = styled.View`
   flex: 1;
+`;
+
+const Loader = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BgImg = styled.Image``;
+
+const Title = styled.Text`
+  font-size: 16px;
+  font-weight: 600;
+  color: white;
+`;
+
+const Poster = styled.Image`
+  width: 100px;
+  height: 160px;
+  border-radius: 5px;
+`;
+
+const Wrapper = styled.View`
+  flex-direction: row;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Column = styled.View`
+  width: 40%;
+  margin-left: 15px;
+`;
+
+const Overview = styled.Text`
+  margin-top: 10px;
+  color: rgba(255, 255, 255, 0.8);
+`;
+
+const Votes = styled(Overview)`
+  margin-top: 5px;
+  font-size: 12px;
 `;
