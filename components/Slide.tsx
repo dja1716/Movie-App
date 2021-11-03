8eea7448756f604063f5e12ad6c9200a9620a18e
dja1716 <hdum@uwaterloo.ca>
2021-11-03 13:53:24 +0900

    refactor

---
+++ components/Slide.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import styled from "styled-components/native";
+import { StyleSheet, useColorScheme } from "react-native";
+import { makeImgPath } from "../utils";
+import { BlurView } from "expo-blur";
+import Poster from "./Poster";
+
+interface SlideProps {
+  backdropPath: string;
+  posterPath: string;
+  originalTitle: string;
+  voteAverage: number;
+  overview: string;
+}
+
+const Slide: React.FC<SlideProps> = ({
+  backdropPath,
+  posterPath,
+  originalTitle,
+  voteAverage,
+  overview,
+}) => {
+  const isDark = useColorScheme() === "dark";
+
+  return (
+    <View>
+      <BgImg
+        style={StyleSheet.absoluteFill}
+        source={{ uri: makeImgPath(backdropPath) }}
+      />
+      <BlurView
+        tint={isDark ? "dark" : "light"}
+        intensity={80}
+        style={StyleSheet.absoluteFill}
+      >
+        <Wrapper>
+          <Poster path={posterPath} />
+          <Column>
+            <Title isDark={isDark}>{originalTitle}</Title>
+            {voteAverage > 0 ? (
+              <Votes isDark={isDark}>⭐{voteAverage}/10</Votes>
+            ) : null}
+            <Overview isDark={isDark}>{overview.slice(0, 90)}...</Overview>
+          </Column>
+        </Wrapper>
+      </BlurView>
+    </View>
+  );
+};
+
+const View = styled.View`
+  flex: 1;
+`;
+
+const BgImg = styled.Image``;
+
+const Title = styled.Text<{ isDark: boolean }>`
+  font-size: 16px;
+  font-weight: 600;
+  color: ${(props) => (props.isDark ? "white" : props.theme.textColor)};
+`;
+
+
+
+const Wrapper = styled.View`
+  flex-direction: row;
+  height: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Column = styled.View`
+  width: 40%;
+  margin-left: 15px;
+`;
+
+const Overview = styled.Text<{ isDark: boolean }>`
+  margin-top: 10px;
+  color: ${(props) =>
+    props.isDark ? "rgba(255, 255, 255, 0.8)" : "rgba 0, 0, 0, 0.8)"};
+`;
+
+const Votes = styled(Overview)`
+  margin-top: 5px;
+  font-size: 12px;
+`;
+
+export default Slide;
