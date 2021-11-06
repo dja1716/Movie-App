b6be97f95e9735bafbddc950654ce40f9cdd2141
dja1716 <hdum@uwaterloo.ca>
2021-11-06 18:34:40 +0900

    search

--- api.ts
+++ api.ts
@@ -18,17 +18,35 @@ export interface Movie {
   vote_count: number;
 }
 
+export interface TV {
+  name: string;
+  original_name: string;
+  origin_country: string[];
+  vote_count: number;
+  backdrop_path: string | null;
+  vote_average: number;
+  genre_ids: number[];
+  id: number;
+  original_language: string;
+  overview: string;
+  poster_path: string | null;
+  first_air_date: string;
+  popularity: number;
+  media_type: string;
+}
+
 interface BaseResponse {
   page: number;
   total_results: number;
   total_pages: number;
 }
+export interface TVResponse extends BaseResponse {
+  results: TV[];
+}
 
 export interface MovieResponse extends BaseResponse {
   results: Movie[];
 }
-
-
 
 export const moviesApi = {
   trending: () =>
@@ -42,9 +60,15 @@ export const moviesApi = {
   nowPlaying: () =>
     fetch(
       `${BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`
-    ).then((res) => res.json())
+    ).then((res) => res.json()),
+  //@ts-ignore
+  search: ({ queryKey }) => {
+    const [_, query] = queryKey;
+    return fetch(
+      `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&page=1&query=${query}`
+    ).then((res) => res.json());
+  },
 };
-
 
 export const tvApi = {
   trending: () =>
@@ -59,4 +83,11 @@ export const tvApi = {
     fetch(`${BASE_URL}/tv/top_rated?api_key=${API_KEY}`).then((res) =>
       res.json()
     ),
+  //@ts-ignore
+  search: ({ queryKey }) => {
+    const [_, query] = queryKey;
+    return fetch(
+      `${BASE_URL}/search/tv?api_key=${API_KEY}&language=en-US&page=1&query=${query}`
+    ).then((res) => res.json());
+  },
 };
--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,9 +1,32 @@
 import React, { useState } from "react";
-import styled from "styled-components/native"
+import { useQuery } from "react-query";
+import styled from "styled-components/native";
+import { moviesApi, tvApi } from "../api";
+import HList from "../components/HList";
+import Loader from "../components/Loader";
 
 const Search = () => {
   const [query, setQuery] = useState("");
-  const onChangeText = (text: string) => { setQuery(text); }
+  const {
+    isLoading: moviesLoading,
+    data: moviesData,
+    refetch: searchMovies,
+  } = useQuery(["searchMovies", query], moviesApi.search, { enabled: false });
+  const { isLoading: tvLoading, data: tvData, refetch: searchTv } = useQuery(
+    ["searchTv", query],
+    tvApi.search,
+    { enabled: false }
+  );
+  const onChangeText = (text: string) => {
+    setQuery(text);
+  };
+  const onSubmit = () => {
+    if (query === "") {
+      return;
+    }
+    searchMovies();
+    searchTv();
+  };
   return (
     <Container>
       <SearchBar
@@ -11,10 +34,16 @@ const Search = () => {
         placeholderTextColor="gray"
         returnKeyType="search"
         onChangeText={onChangeText}
+        onSubmitEditing={onSubmit}
       />
+      {moviesLoading || tvLoading ? <Loader /> : null}
+      {moviesData ? (
+        <HList title="Movie Results" data={moviesData.results} />
+      ) : null}
+      {tvData ? <HList title="TV Results" data={tvData.results} /> : null}
     </Container>
   );
-}
+};
 
 export default Search;
 
@@ -26,4 +55,5 @@ const SearchBar = styled.TextInput`
   border-radius: 15px;
   width: 90%;
   margin: 10px auto;
+  margin-bottom: 40px;
 `;
