a3b8ac77cdd53df62cd27e8dbecb96c8390db0a1
dja1716 <hdum@uwaterloo.ca>
2021-11-07 17:17:55 +0900

    Detail Apit

--- api.ts
+++ api.ts
@@ -1,3 +1,5 @@
+import { QueryFunction } from "react-query";
+
 const API_KEY = "356723332b0147990f77bce41e0a96bb";
 const BASE_URL = "https://api.themoviedb.org/3";
 
@@ -48,7 +50,11 @@ export interface MovieResponse extends BaseResponse {
   results: Movie[];
 }
 
-export const moviesApi = {
+interface Fetchers<T> {
+  [key: string]: QueryFunction<T>;
+}
+
+export const moviesApi: Fetchers<MovieResponse> = {
   trending: () =>
     fetch(`${BASE_URL}/trending/movie/week?api_key=${API_KEY}`).then((res) =>
       res.json()
@@ -61,16 +67,21 @@ export const moviesApi = {
     fetch(
       `${BASE_URL}/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=KR`
     ).then((res) => res.json()),
-  //@ts-ignore
   search: ({ queryKey }) => {
     const [_, query] = queryKey;
     return fetch(
       `${BASE_URL}/search/movie?api_key=${API_KEY}&language=en-US&page=1&query=${query}`
     ).then((res) => res.json());
   },
+  detail: ({ queryKey }) => {
+    const [_, id] = queryKey;
+    return fetch(
+      `${BASE_URL}/movie/${id}?api_key=${API_KEY}&append_to_response=videos,images`
+    ).then((res) => res.json());
+  },
 };
 
-export const tvApi = {
+export const tvApi: Fetchers<TVResponse> = {
   trending: () =>
     fetch(`${BASE_URL}/trending/tv/week?api_key=${API_KEY}`).then((res) =>
       res.json()
@@ -83,11 +94,16 @@ export const tvApi = {
     fetch(`${BASE_URL}/tv/top_rated?api_key=${API_KEY}`).then((res) =>
       res.json()
     ),
-  //@ts-ignore
   search: ({ queryKey }) => {
     const [_, query] = queryKey;
     return fetch(
       `${BASE_URL}/search/tv?api_key=${API_KEY}&language=en-US&page=1&query=${query}`
+    ).then((res) => res.json());
+  },
+  detail: ({ queryKey }) => {
+    const [_, id] = queryKey;
+    return fetch(
+      `${BASE_URL}/tv/${id}?api_key=${API_KEY}&append_to_response=videos,images`
     ).then((res) => res.json());
   },
 };
--- screens/Detail.tsx
+++ screens/Detail.tsx
@@ -1,9 +1,13 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useEffect } from "react";
-import { Text, View } from "react-native";
 import styled from "styled-components/native";
-import { Movie, TV } from "../api";
+import { Movie, moviesApi, TV, tvApi } from "../api";
 import Poster from "../components/Poster";
+import { Dimensions, StyleSheet } from "react-native";
+import { makeImgPath } from "../utils";
+import { LinearGradient } from "expo-linear-gradient";
+import { BLACK_COLOR } from "../colors";
+import { useQuery } from "react-query";
 
 type RootStackParamList = {
   Detail: Movie | TV;
@@ -11,21 +15,54 @@ type RootStackParamList = {
 
 type DetailScreenProps = NativeStackScreenProps<RootStackParamList, "Detail">;
 
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
+
 const Detail: React.FC<DetailScreenProps> = ({
   navigation: { setOptions },
   route: { params },
 }) => {
+  const { isLoading: moviesLoading, data: moviesData } = useQuery(
+    ["movies", params.id],
+    moviesApi.detail,
+    {
+      enabled: "original_title" in params,
+    }
+  );
+  const { isLoading: tvLoading, data: tvData } = useQuery(
+    ["tv", params.id],
+    tvApi.detail,
+    {
+      enabled: "original_name" in params,
+    }
+  );
+
   useEffect(() => {
     setOptions({
-      title:
-        "original_title" in params
-          ? params.original_title
-          : params.original_name,
+      title: "original_title" in params ? "Movie" : "TV Show",
     });
   }, []);
   return (
     <Container>
-      <Poster path={params.poster_path || ""} />
+      <Header>
+        <Background
+          style={StyleSheet.absoluteFill}
+          source={{ uri: makeImgPath(params.backdrop_path || "") }}
+        />
+        <LinearGradient
+          // Background Linear Gradient
+          colors={["transparent", BLACK_COLOR]}
+          style={StyleSheet.absoluteFill}
+        />
+        <Column>
+          <Poster path={params.poster_path || ""} />
+          <Title>
+            {"original_title" in params
+              ? params.original_title
+              : params.original_name}
+          </Title>
+        </Column>
+      </Header>
+      <Overview>{params.overview}</Overview>
     </Container>
   );
 };
@@ -34,4 +71,30 @@ export default Detail;
 
 const Container = styled.ScrollView`
   background-color: ${(props) => props.theme.mainBgColor};
+`;
+
+const Header = styled.View`
+  height: ${SCREEN_HEIGHT / 4}px;
+  justify-content: flex-end;
+  padding: 0px 20px;
+`;
+
+const Background = styled.Image``;
+
+const Column = styled.View`
+  flex-direction: row;
+  width: 80%;
+`;
+const Title = styled.Text`
+  color: white;
+  font-size: 36px;
+  align-self: flex-end;
+
+  margin-left: 15px;
+  font-weight: 500;
+`;
+const Overview = styled.Text`
+  color: ${(props) => props.theme.textColor};
+  margin-top: 20px;
+  padding: 0px 20px;
 `;
