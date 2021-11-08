11327ae339f0e49d4b23703c6d865a83270c5f37
dja1716 <hdum@uwaterloo.ca>
2021-11-08 12:38:47 +0900

    share

--- screens/Detail.tsx
+++ screens/Detail.tsx
@@ -3,11 +3,20 @@ import React, { useEffect } from "react";
 import styled from "styled-components/native";
 import { Movie, moviesApi, TV, tvApi } from "../api";
 import Poster from "../components/Poster";
-import { Dimensions, StyleSheet } from "react-native";
+import {
+  Dimensions,
+  StyleSheet,
+  Linking,
+  TouchableOpacity,
+  Share,
+  Platform,
+} from "react-native";
 import { makeImgPath } from "../utils";
 import { LinearGradient } from "expo-linear-gradient";
 import { BLACK_COLOR } from "../colors";
 import { useQuery } from "react-query";
+import Loader from "../components/Loader";
+import { Ionicons } from "@expo/vector-icons";
 
 type RootStackParamList = {
   Detail: Movie | TV;
@@ -21,19 +30,10 @@ const Detail: React.FC<DetailScreenProps> = ({
   navigation: { setOptions },
   route: { params },
 }) => {
-  const { isLoading: moviesLoading, data: moviesData } = useQuery(
-    ["movies", params.id],
-    moviesApi.detail,
-    {
-      enabled: "original_title" in params,
-    }
-  );
-  const { isLoading: tvLoading, data: tvData } = useQuery(
-    ["tv", params.id],
-    tvApi.detail,
-    {
-      enabled: "original_name" in params,
-    }
+  const isMovie = "original_title" in params;
+  const { isLoading, data } = useQuery(
+    [isMovie ? "movies" : "tv", params.id],
+    isMovie ? moviesApi.detail : tvApi.detail
   );
 
   useEffect(() => {
@@ -41,6 +41,53 @@ const Detail: React.FC<DetailScreenProps> = ({
       title: "original_title" in params ? "Movie" : "TV Show",
     });
   }, []);
+
+  const openYTLink = async (videoID: string) => {
+    const baseUrl = `https://m.youtube.com/watch?v=${videoID}`;
+    await Linking.openURL(baseUrl);
+    //await WebBrowser.openBrowserAsync(baseUrl);
+  };
+
+  useEffect(() => {
+    if (data) {
+      setOptions({
+        headerRight: () => <ShareButton />,
+      });
+    }
+  }, [data]);
+
+  const ShareButton = () => (
+    <TouchableOpacity onPress={shareMedia}>
+      <Ionicons name="share-outline" color="white" size={24} />
+    </TouchableOpacity>
+  );
+
+  const shareMedia = async () => {
+    const isAndroid = Platform.OS === "android";
+
+    const homepage = isMovie
+      ? `https://www.imdb.com/title/${data.imdb_id}/`
+      : //@ts-ignore
+        data.homepage;
+    if (isAndroid) {
+      await Share.share({
+        message: `${params.overview}\nCheck it out: ${homepage}`,
+        title:
+          "original_title" in params
+            ? params.original_title
+            : params.original_name,
+      });
+    } else {
+      await Share.share({
+        url: homepage,
+        title:
+          "original_title" in params
+            ? params.original_title
+            : params.original_name,
+      });
+    }
+  };
+
   return (
     <Container>
       <Header>
@@ -62,12 +109,40 @@ const Detail: React.FC<DetailScreenProps> = ({
           </Title>
         </Column>
       </Header>
-      <Overview>{params.overview}</Overview>
+      <Data>
+        <Overview>{params.overview}</Overview>
+        {isLoading ? <Loader /> : null}
+        {
+          //@ts-ignore
+          data?.videos?.results?.map((video) => (
+            <VideoBtn key={video.key} onPress={() => openYTLink(video.key)}>
+              <Ionicons name="logo-youtube" color="white" size={24} />
+              <BtnText>{video.name}</BtnText>
+            </VideoBtn>
+          ))
+        }
+      </Data>
     </Container>
   );
 };
 
 export default Detail;
+
+const Data = styled.View`
+  padding: 0 20px;
+`;
+
+const VideoBtn = styled.TouchableOpacity`
+  flex-direction: row;
+`;
+
+const BtnText = styled.Text`
+  color: white;
+  font-weight: 600;
+  margin-bottom: 10px;
+  line-height: 24px;
+  margin-left: 10px;
+`;
 
 const Container = styled.ScrollView`
   background-color: ${(props) => props.theme.mainBgColor};
@@ -95,6 +170,6 @@ const Title = styled.Text`
 `;
 const Overview = styled.Text`
   color: ${(props) => props.theme.textColor};
-  margin-top: 20px;
+  margin: 20px 0;
   padding: 0px 20px;
 `;
